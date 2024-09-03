66a9137f584d26b74d1024c4e36e5551bee7705c
Vivek Raj <43422488+techyvivekraj@users.noreply.github.com>
2024-09-03 22:11:09 +0530

    Update constants.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const String apiKey = "7647a0a758efcd293d48b84cbe31295a";
+  static const String apiKey = "apiKey";
   static const String baseUrl = "https://api.themoviedb.org/3/movie/";
   static const String popularMovie = "popular";
   static const String topRatedMovie = "top_rated";
